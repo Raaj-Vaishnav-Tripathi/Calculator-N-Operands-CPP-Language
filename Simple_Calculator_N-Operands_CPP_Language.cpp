37510b65bca4262a18ebca5c3c08eded0ca50ba9
Raj Vaishnav Tripathi <118334052+Raj-Vaishnav-Tripathi@users.noreply.github.com>
2023-09-09 23:15:59 +0530

    Simple_Calculator_N-Operands_CPP_Language.cpp

--- Simple_Calculator_N-Operands_CPP_Language.cpp
+++ Simple_Calculator_N-Operands_CPP_Language.cpp
@@ -1,75 +1,75 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-  char operator; // For taking the Operator input in character variable.
-  int n, i; // For the size of Operand array and i for iteration of loop.
-  double extra = 0; // For saving the updated values after performing selected operations or at intial stage for storing 1st value.
+    char oper; // For taking the operator input in a character variable.
+    int n, i; // For the size of Operand array and i for iteration of the loop.
+    double extra = 0; // For saving the updated values after performing selected operations or at the initial stage for storing the 1st value.
 
-  cout << "Enter an operator (+, -, *, /): ";
-  cin >> operator;
+    std::cout << "Enter an operator (+, -, *, /): ";
+    std::cin >> oper;
 
-  cout << "Enter Number of Operands: ";
-  cin >> n;
-  double operands[n]; // Operand Array for taking the input of N Operands and size of array is already provided by user as N is size of Array.
-  cout << "Enter " << n << " operands below:-\n";
-  for (i = 0; i < n; i++) {
-    cin >> operands[i];
-  }
+    std::cout << "Enter Number of Operands: ";
+    std::cin >> n;
+    double operands[n]; // Operand Array for taking the input of N Operands, and the size of the array is already provided by the user as N is the size of the Array.
 
-  switch (operator) // Here, I Used Switch Cases for all 4 operations and added defination of all 4 Arithematic Operations.
-  {
-  case '+':
+    std::cout << "Enter " << n << " operands below:" << std::endl;
     for (i = 0; i < n; i++) {
-      if (i < n - 1) {
-        cout << operands[i] << " + ";
-        extra += operands[i];
-      } else {
-        cout << operands[i];
-        extra += operands[i];
-        cout << " = " << extra;
-      }
+        std::cin >> operands[i];
     }
-    break;
-  case '-':
-    for (i = 0; i < n; i++) {
-      if (i == 0) {
-        cout << operands[i] << " - ";
-        extra += operands[i];
-      } else if (i < n - 1) {
-        cout << operands[i] << " - ";
-        extra -= operands[i];
-      } else {
-        cout << operands[i];
-        extra -= operands[i];
-        cout << " = " << extra;
-      }
+
+    switch (oper) {
+    case '+':
+        for (i = 0; i < n; i++) {
+            if (i < n - 1) {
+                std::cout << operands[i] << " + ";
+                extra = extra + operands[i];
+            } else {
+                std::cout << operands[i];
+                extra = extra + operands[i];
+                std::cout << " = " << extra;
+            }
+        }
+        break;
+    case '-':
+        for (i = 0; i < n; i++) {
+            if (i == 0) {
+                std::cout << operands[i] << " - ";
+                extra = extra + operands[i];
+            } else if (i < n - 1) {
+                std::cout << operands[i] << " - ";
+                extra = extra - operands[i];
+            } else {
+                std::cout << operands[i];
+                extra = extra - operands[i];
+                std::cout << " = " << extra;
+            }
+        }
+        break;
+    case '*':
+        for (i = 0; i < n; i++) {
+            if (i == 0) {
+                std::cout << operands[i] << " x ";
+                extra = extra + operands[i];
+            } else if (i < n - 1) {
+                std::cout << operands[i] << " x ";
+                extra = extra * operands[i];
+            } else {
+                std::cout << operands[i];
+                extra = extra * operands[i];
+                std::cout << " = " << extra;
+            }
+        }
+        break;
+    case '/':
+        extra = operands[0];
+        for (i = 0; i < n - 1; i++) {
+            extra = extra / operands[i + 1];
+            std::cout << extra << " ";
+        }
+        break;
+    default:
+        std::cout << "Error! Operator is not correct";
     }
-    break;
-  case '*':
-    for (i = 0; i < n; i++) {
-      if (i == 0) {
-        cout << operands[i] << " x ";
-        extra += operands[i];
-      } else if (i < n - 1) {
-        cout << operands[i] << " x ";
-        extra *= operands[i];
-      } else {
-        cout << operands[i];
-        extra *= operands[i];
-        cout << " = " << extra;
-      }
-    }
-    break;
-  case '/':
-    extra = operands[0];
-    for (i = 0; i < n - 1; i++) {
-      extra = extra / operands[i + 1];
-      cout << extra << " ";
-    }
-    break;
-  default:
-    cout << "Error! operator is not correct";
-  }
-  return 0;
+
+    return 0;
 }
